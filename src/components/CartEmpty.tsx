b9514d57dd56f774c70be26b195cba16af029a7f
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-10 03:18:08 -0300

    Changed style components CartEmpty

--- src/components/CartEmpty.tsx
+++ src/components/CartEmpty.tsx
@@ -4,12 +4,12 @@ export function CartEmpty() {
 
   return (
     <div className="flex flex-col items-center justify-center">
-    <i className="text-[#F30000] text-8xl fa-solid fa-cart-shopping mb-5"></i>
-    <h1 className="text-lg font-normal text-[#F30000]">Your cart is empty</h1>
-    <h2 className="text-lg font-normal">Add a product clicked at botton "add to cart" from home page.</h2>
-    <Link
-      className="mt-5 px-5 py-2 border-2 border-[#F30000] text-[#F30000] cursor-pointer"
-      to='/'>Return from home page</Link>
-  </div>
+      <i className="text-[#F30000] text-7xl fa-solid fa-cart-shopping mb-5"></i>
+      <h1 className="text-center text-lg font-normal text-[#F30000]">Your cart is empty</h1>
+      <h2 className="text-center text-base font-normal">Add a product clicked at botton "add to cart" from home page.</h2>
+      <Link
+        className="flex items-center justify-center mt-5 px-5 py-2 border-2 border-[#F30000] text-center text-[#F30000] cursor-pointer"
+        to='/'>Return from home page</Link>
+    </div>
   )
 }
