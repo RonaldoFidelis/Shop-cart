3666fb11a7f2055256164f0eb407b1ebf7914f78
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-10 02:43:59 -0300

    Feature: Create page cart

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,18 @@
+import { useContext } from "react";
+import { CartContext } from "../context/CartContext";
+import { CartEmpty } from "../components/CartEmpty";
+import { CartNotEmpty } from "../components/CartNotEmpty";
+
 export function Cart() {
+  const {cart} = useContext(CartContext);
+
   return (
-    <div>
-      Carrinho
+    <div className="w-full min-h-screen py-20">
+      {cart.length === 0 ? (
+        <CartEmpty/>
+      ):(
+        <CartNotEmpty/>
+      )}
     </div>
   )
 }
