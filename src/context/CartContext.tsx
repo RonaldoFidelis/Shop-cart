44211ba4e1ea999c82f08e44deecaed868ae73cb
RonaldoFidelis <ronaldofidelis.ti@gmail.com>
2023-11-11 01:00:51 -0300

    Fix: error type 'FormatData' is not assignable to parameter

--- src/context/CartContext.tsx
+++ src/context/CartContext.tsx
@@ -1,6 +1,16 @@
 import React, { createContext, useState, ReactNode } from "react";
 
-type FormatData = {
+type FormatCart = {
+  img: string;
+  name: string;
+  color: string;
+  price: number;
+  size: number[];
+  id: string;
+  quantity: number;
+}
+
+type FormatFavorite = {
   img: string;
   name: string;
   color: string;
@@ -11,10 +21,10 @@ type FormatData = {
 }
 
 type CartContextType = {
-  cart: FormatData[];
-  setCart: React.Dispatch<React.SetStateAction<FormatData[]>>;
-  favorite: FormatData[];
-  setFavorite: React.Dispatch<React.SetStateAction<FormatData[]>>;
+  cart: FormatCart[];
+  setCart: React.Dispatch<React.SetStateAction<FormatCart[]>>;
+  favorite: FormatFavorite[];
+  setFavorite: React.Dispatch<React.SetStateAction<FormatFavorite[]>>;
 };
 
 export const CartContext = createContext<CartContextType>({} as CartContextType);
@@ -24,8 +34,8 @@ type CartContextProviderProps = {
 };
 
 export const CartContextProvider: React.FC<CartContextProviderProps> = ({ children }) => {
-  const [cart, setCart] = useState<FormatData[]>([]);
-  const [favorite, setFavorite] = useState<FormatData[]>([]);
+  const [cart, setCart] = useState<FormatCart[]>([]);
+  const [favorite, setFavorite] = useState<FormatFavorite[]>([]);
 
   return (
     <CartContext.Provider value={{ cart, setCart, favorite, setFavorite }}>
